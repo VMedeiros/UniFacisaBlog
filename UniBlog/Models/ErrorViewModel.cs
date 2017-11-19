15d105596ca7a148e9a6c059caff7d8b6ede6dca
Victor Medeiros <victor.leitao95@gmail.com>
2017-11-19 19:55:47 -0300

    Unifacisa Blog

---
+++ UniBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UniBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
