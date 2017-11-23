cd048cb5ee289be564214f211179c3e5f0be5359
Victor Medeiros <victor.leitao95@gmail.com>
2017-11-22 21:54:05 -0300

    UniFacisa Blog

---
+++ UniBlog/Controllers/ComentariosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UniBlog.Dados;
+using UniBlog.Models;
+
+namespace UniBlog.Controllers
+{
+    public class ComentariosController : Controller
+    {
+        private readonly BlogContext _context;
+
+        public ComentariosController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Comentarios
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Comentarios.ToListAsync());
+        }
+
+        // GET: Comentarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentario = await _context.Comentarios
+                .SingleOrDefaultAsync(m => m.ComentarioID == id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentario);
+        }
+
+        // GET: Comentarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Comentarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ComentarioID,Texto,DataCadastro2")] Comentario comentario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(comentario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(comentario);
+        }
+
+        // GET: Comentarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentario = await _context.Comentarios.SingleOrDefaultAsync(m => m.ComentarioID == id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+            return View(comentario);
+        }
+
+        // POST: Comentarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ComentarioID,Texto,DataCadastro2")] Comentario comentario)
+        {
+            if (id != comentario.ComentarioID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comentario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ComentarioExists(comentario.ComentarioID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(comentario);
+        }
+
+        // GET: Comentarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentario = await _context.Comentarios
+                .SingleOrDefaultAsync(m => m.ComentarioID == id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentario);
+        }
+
+        // POST: Comentarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var comentario = await _context.Comentarios.SingleOrDefaultAsync(m => m.ComentarioID == id);
+            _context.Comentarios.Remove(comentario);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ComentarioExists(int id)
+        {
+            return _context.Comentarios.Any(e => e.ComentarioID == id);
+        }
+    }
+}
